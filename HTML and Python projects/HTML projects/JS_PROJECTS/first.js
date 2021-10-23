afd3596a2229cfcb40cf7c7994512173e1fb3c8b
9thAvatar <86769556+9thAvatar@users.noreply.github.com>
2021-10-23 11:54:00 +0400

    Added some fresh files!

---
+++ HTML and Python projects/HTML projects/JS_PROJECTS/first.js
@@ -0,0 +1,7 @@
+function add() {
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+    var sum = Number(n1) + Number(n2)
+    alert(sum)
+}
+//This adds the numbers
